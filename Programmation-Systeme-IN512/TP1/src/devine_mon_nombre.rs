e6ebfd77c977308bee10722439766e2907943003
Ayoub <ayoub.ourimi@ens.uvsq.fr>
2024-09-22 21:32:57 +0200

    Ajout du fichier devine_mon_nombre.rs

---
+++ Programmation-Systeme-IN512/TP1/src/devine_mon_nombre.rs
@@ -0,0 +1,38 @@
+//1
+/* fn main() {
+    println!("Hello World!");
+} */
+
+//2
+/* compiler : rustc devine_mon_fichier.rs
+   éxecuter : devine_mon_fichier */
+
+// 3
+
+////////////////////
+
+
+//1
+
+/* fn main(){
+    println!("Devine mon nombre ! \n\nSaisissez votre proposition.");
+} */
+
+//2.ii
+use std::io;  //import input
+
+fn main(){
+    println!("Devine mon nombre ! \n\nSaisissez votre proposition.");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input);
+
+    println!("Votre nombre est : {input}")
+}
+
+//2.iii
+/* On fournit une variable prete à etre modifier (ici une reference mutable --> &mut) pour stocker la ligne lue*/
+
+//2iv
+ /* Result<usize> --> utilisé pour gerer les erreur --> moyen de retourner soit un succes (ici un usize) soit une erreur */
